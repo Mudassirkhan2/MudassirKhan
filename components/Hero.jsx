3620de160689094f392d5e1564f5a1d080e19501
Mohammed Mudassir ullah Khan <mudassir222001@gmail.com>
2023-05-31 13:03:04 +0530

    added contact section and fixed minnor issues

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -5,36 +5,61 @@ import linkedinIcon from '../app/assets/linkedin.png';
 import twitterIcon from '../app/assets/twitter.png';
 import {motion} from 'framer-motion'
 import Image from 'next/image';
-import { useState } from 'react';
+import { useState ,useEffect } from 'react';
 import Intro from './clients/Intro';
 
 const Hero = () => {
+    const [animationComplete, setAnimationComplete] = useState(true);
     const [textSize, setTextSize] = useState("7rem");
+    const [textSizeMobile, setTextSizeMobile] = useState("2rem");
     const [transitiontime, setTransitiontime] = useState(3);
-    const [yValue, setYValue] = useState("30vh");
+    const [yValue, setYValue] = useState("20vh");
+    const [fontFamily, setFontFamily] = useState("nabla");
+    useEffect(() => {
+        if (animationComplete) {
+          document.body.style.overflow = 'auto'; // Enable scrolling
+        } else {
+          document.body.style.overflow = 'hidden'; // Disable scrolling
+        }
+      }, [animationComplete]);
+      const isLargeDevice = window.innerWidth > 768; // 768 is the min-width for lg devices
     // to change the text size after animation complete
     // to change the transition time after animation complete
     const handleAnimationComplete = () => {
-        setTextSize("5rem");
         setTransitiontime(0.3);
         setYValue("0vh");
+        setFontFamily("jost");
+        setAnimationComplete(true);
+        if(isLargeDevice){
+            setTextSize("5rem");
+        }
+        else {
+            setTextSizeMobile("2rem");
+        }
     };
+   
   return (
     <div className="flex flex-col relative items-center justify-center w-full h-[100vh] overflow-hidden select-none">
         <div className='max-w-6xl p-2 rounded-lg shadow-2xl'>
-            <div className='absolute top-0 cursor-pointer left-[66%] animate-pulse'>
-                <a href="">
+            <div className='absolute top-0 cursor-pointer left-[66%]  hidden lg:block'>
+                <a href="https://github.com/Mudassirkhan2"
+                    target="_blank" rel="noopener noreferrer" className="cursor-pointer "
+                >
                     <Image
                             src={GithubIcon}
                             alt="Picture of the author"
                             width={70}
                             height={70}
                             className="rounded-full"
-                            ></Image>
+                            >
+
+                            </Image>
                 </a>
             </div>
-            <div className='absolute top-0 cursor-pointer left-1/3 '>
-            <a href=""> 
+            <div className='absolute top-0 hidden cursor-pointer left-1/3 lg:block'>
+            <a 
+                href="mailto:mudassir222001@gmail.com " target="_blank" rel="noopener noreferrer" className="cursor-pointer "
+            > 
                     <Image
                             src={gmailIcon}
                             alt="Picture of the author"
@@ -44,8 +69,10 @@ const Hero = () => {
                             ></Image>
                 </a>
             </div>
-            <div className='absolute left-0 cursor-pointer top-3/4 ' >
-                <a href="https://www.linkedin.com/in/mudassir-khan-522303233/">
+            <div className='absolute left-0 hidden cursor-pointer top-3/4 lg:block' >
+                <a href="https://www.linkedin.com/in/mudassir-khan-522303233/"
+                    target="_blank" rel="noopener noreferrer" className="cursor-pointer "
+                >
                         <Image
                             src={linkedinIcon}
                             alt="Picture of the author"
@@ -55,8 +82,10 @@ const Hero = () => {
                         ></Image>
                     </a>
             </div>
-            <div className='absolute right-0 cursor-pointer top-1/4 '>
-                <a href="">
+            <div className='absolute right-0 hidden cursor-pointer top-1/4 lg:block '>
+                <a href="https://twitter.com/Mudassir_222"
+                    target="_blank" rel="noopener noreferrer" className="cursor-pointer "
+                >
                     <Image
                             src={twitterIcon}
                             alt="Picture of the author"
@@ -67,19 +96,46 @@ const Hero = () => {
                 </a>
             </div>
             <div className="flex flex-col w-full">
-                <motion.h1 className="z-30 text-teal-300 text-start whitespace-nowrap " 
-                    initial={{ x:"100vw",
-                        y:"30vh",
-                        fontSize:"7rem",
-                    }}
-                    animate={{ x:0,
-                        y:yValue,
-                        fontSize:textSize,
-                    }}
-                    transition={{duration:transitiontime}}
-                    onAnimationComplete={handleAnimationComplete}
-                > <span className='outline-text' style={{ whiteSpace: "nowrap" }}>Hello! i&apos;m </span>  Mudassir Khan </motion.h1>
-                <p className="mt-3 mb-8 text-4xl text-center"> a
+                {   
+                    isLargeDevice  ?    ( 
+                            <motion.h1 className="z-30 text-teal-300 text-start whitespace-nowrap font-nabla " 
+                                initial={{ x:"100vw",
+                                    y:"20vh",
+                                    fontSize:"7rem",
+                                    fontFamily:"nabla",
+                                }}
+                                animate={{ x:0,
+                                    y:yValue,
+                                    fontSize:textSize,
+                                    fontFamily:fontFamily,
+                                }}
+                                transition={{duration:transitiontime}}
+                                onAnimationComplete={handleAnimationComplete}
+                            > 
+                                    <span className='outline-text' style={{ whiteSpace: "nowrap" }}>Hello! i&apos;m </span>  Mudassir Khan
+                            </motion.h1> 
+
+                ) : (
+                    <motion.h1 className="z-30 text-teal-300 text-start whitespace-nowrap font-nabla " 
+                                initial={{ x:"100vw",
+                                    y:"20vh",
+                                    fontSize:"2rem",
+                                    fontFamily:"nabla",
+                                }}
+                                animate={{ x:0,
+                                    y:yValue,
+                                    fontSize:textSizeMobile,
+                                    fontFamily:fontFamily,
+                                }}
+                                transition={{duration:transitiontime}}
+                                onAnimationComplete={handleAnimationComplete}
+                            > 
+                                    <span className='outline-text' style={{ whiteSpace: "nowrap" }}>Hello! i&apos;m </span>  Mudassir Khan
+                            </motion.h1> 
+
+                )
+            }
+                <p className="mt-3 mb-8 text-2xl text-center md:text-4xl"> a
                     <span className="text-red-500 ">  Frontend Developer  </span>From Hyderabad
                 </p>
         </div>
