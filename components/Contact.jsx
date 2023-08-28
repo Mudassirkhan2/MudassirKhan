b0a56da0f47bd74803a1f322d298e4a89a23b9a9
Mohammed Mudassir ullah Khan <mudassir222001@gmail.com>
2023-08-28 12:25:44 +0530

    optimized HomeFinder gif

--- components/Contact.jsx
+++ components/Contact.jsx
@@ -8,28 +8,28 @@ import { ElementWrapper } from './clients/client';
 const Contact = () => {
   return (
     <ElementWrapper nameOfTheElement="contact">
-    <section className='max-w-6xl p-2 mx-auto mt-10 '>
+      <section className='max-w-6xl p-2 mx-auto mt-10 '>
         <div className='flex justify-center w-full mb-8 gap-x-10'>
-            <a href="https://www.linkedin.com/in/mudassir-khan-522303233/"
-              target="_blank"  className="cursor-pointer "
-            >
-              <Image src={linkedinIcon} alt='contact' width={50} height={50} />
-            </a>
-            <a href="https://github.com/Mudassirkhan2"
-              target="_blank"  className="cursor-pointer "
-            >
-                <Image src={GithubIcon} alt='contact' width={50} height={50} /> 
-            </a>
-            <a href="mailto:mudassir222001@gmail.com " target="_blank"  className="cursor-pointer ">
-              <Image src={gmailIcon} alt='contact' width={50} height={50} />
-            </a>
-            <a href="https://twitter.com/Mudassir_222" 
-              target="_blank"  className="cursor-pointer "
-            >
-              <Image src={twitterIcon} alt='contact' width={50} height={50} />
-            </a>
+          <a href="https://www.linkedin.com/in/mudassir-khan-522303233/"
+            target="_blank" className="cursor-pointer "
+          >
+            <Image src={linkedinIcon} alt='contact' width={50} height={50} loading='lazy' />
+          </a>
+          <a href="https://github.com/Mudassirkhan2"
+            target="_blank" className="cursor-pointer "
+          >
+            <Image src={GithubIcon} alt='contact' width={50} height={50} loading='lazy' />
+          </a>
+          <a href="mailto:mudassir222001@gmail.com " target="_blank" className="cursor-pointer ">
+            <Image src={gmailIcon} alt='contact' width={50} height={50} />
+          </a>
+          <a href="https://twitter.com/Mudassir_222"
+            target="_blank" className="cursor-pointer "
+          >
+            <Image src={twitterIcon} alt='contact' width={50} height={50} loading='lazy' />
+          </a>
         </div>
-    </section>
+      </section>
     </ElementWrapper>
   )
 }
